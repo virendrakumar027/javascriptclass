d3484e26e1444bf031fa4a0a413291e5da050e71
virendrakumar027 <kumarvk0206@gmail.com>
2024-06-08 11:48:16 +0530

    basic js

---
+++ index.js
@@ -0,0 +1,93 @@
+/*var num = 10;
+{
+    num = 20;
+
+}
+console.log(num)
+
+//let
+let num = 10
+{
+    num1 = 20
+}
+console.log(num1)
+
+//const
+const num2 = 10
+{
+    num2 = 20
+}
+console.log(num2)*/
+
+/*let num1 = 20;
+let num2 = 10;
+
+let sum = num1 + num2;
+
+console.log(sum)
+
+let sub = num1 - num2;
+console.log(sub)
+
+let mul = num1 * num2;
+console.log(mul)
+
+let mod = num1 % num2;
+console.log(mod)
+
+let num1 = 20;
+let num2 = 10;
+
+let sum = num1++ + num1 + num1++
+console.log(sum)*/
+
+let num1 = 5;
+let num2 = 10;
+
+/*let sum = --num1 + ++num2 + num1-- + num2++
+console.log(sum)
+console.log(num1)
+console.log(num2)
+let sum = --num1 + ++num1
+console.log(sum)
+
+let checkin = 10
+let checkout = "10";
+
+console.log(typeof obj)
+console.log(typeof arr)
+console.log(typeof checkin)
+console.log(typeof checkout)
+console.log(checkin == checkout);
+
+
+
+
+console.log(checkin == checkout);
+
+
+let checkIn = "false"
+let checkOut = "10";
+console.log(checkIn);
+
+
+let checkIn = true
+let checkOut = false;
+console.log(!checkOut && !(!checkOut));
+
+
+let checkIn = 10
+let checkOut = 20;
+let result = (checkIn == checkOut)?"yes":"no"
+console.log(result);
+
+
+let a = 10
+let b = 20;
+let c = 30
+let result = (a > b && a > c)?"a":(b > c) ? "b" : "c";
+console.log(result);*/
+
+let a = 18;
+let result = (a > 18)?" not vote":"vote"
+console.log(result)
